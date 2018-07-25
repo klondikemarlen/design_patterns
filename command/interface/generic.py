967d00e29945d8c832df8581139581987906a7c5
Marlen Brunner <klondikemarlen@gmail.com>
2018-07-25 09:43:39 -0700

    Improve formatting of RemoteControl.
    Add get_class_name() function to Command class.

--- command/interface/generic.py
+++ command/interface/generic.py
@@ -5,3 +5,22 @@ class Command(ABC):
     @abstractmethod
     def execute(self):
         pass
+
+    def get_class_name(self):
+        """Return a package.module.class style name variable.
+
+        NOTE: Explanation of str(self.__class__).split("'")[1]
+
+        str(self.__class__) -> "<class 'command.interface.light.LightOnCommand'>"
+
+        But I only want the class name ...
+        self.__class__.__name__ -> 'LightOnCommand'
+        Which isn't enough.
+
+        So I split the first string on "'".
+        str(self.__class__).split("'") -> ['<class ', 'command.interface.light.LightOnCommand', '>']
+        And kept only the index 1 term!
+        str(self.__class__).split("'")[1] -> 'command.interface.light.LightOnCommand'
+        """
+
+        return str(self.__class__).split("'")[1]
--- command/remote.py
+++ command/remote.py
@@ -14,7 +14,17 @@ class SimpleRemoteControl:
         self.off_commands[slot_id].execute()
 
     def __str__(self):
+        """Pretty display of Remote Control."""
+        width = 52
         display = "\n------ Remote Control ------\n"
         for i in range(len(self.on_commands)):
-            display += "[slot {}] {}    {}\n".format(i, self.on_commands[i].__class__.__name__, self.off_commands[i].__class__.__name__)
+            display += "[slot {}] ".format(i)
+            if self.on_commands[i] is not None:
+                display += "{:<{width}}".format(self.on_commands[i].get_class_name(), width=width)
+            else:
+                display += "{:<{width}}".format("EMPTY_SLOT", width=width)
+            if self.off_commands[i] is not None:
+                display += "{}\n".format(self.off_commands[i].get_class_name())
+            else:
+                display += "{}\n".format("EMPTY_SLOT")
         return display
